88f1b52ee9059ecaf8f9af5a437d409a1982f27c
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-05-13 21:38:02 +0530

    ADDED : ability to use functions

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -7,20 +7,17 @@ const IS_FULL_TIME = 2;
 const IS_FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
-let empHrs = 0;
-
-empCheck =  Math.floor(Math.random() * 10) % 3;
-switch(empCheck){
+function getWorkingHours(empCheck){
+    switch(empCheck){
     case IS_PART_TIME:
-             empHrs = IS_PART_TIME_HOURS;
-            break;
+            return IS_PART_TIME_HOURS;
     case IS_FULL_TIME:
-             empHrs = IS_FULL_TIME_HOURS;
-             break;
-    default :
-             empHrs = 0;
-            break;
+            return IS_FULL_TIME_HOURS;
+    default :  
+            return 0;
+    }
 }
 
-let empWage = empHrs * WAGE_PER_HOUR;
+empCheck =  Math.floor(Math.random() * 10) % 3;
+let empWage = getWorkingHours(empCheck) * WAGE_PER_HOUR;
 console.log("Emp Wage :" + empWage);
