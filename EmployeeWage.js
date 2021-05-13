b1131b03519c99a8fd3179ac2eb3a38e9bbe2340
Sainath470 <akulavenkatasainath1997@gmail.com>
2021-05-13 21:15:18 +0530

    ADDED : ability to check employee Wage

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -1,9 +1,26 @@
 console.log("Welcome to the Employee Wage program using Java Script");
+
 const IS_ABSENT = 0;
-let empCheck =  Math.floor(Math.random() * 10) % 2;
-if(empCheck == IS_ABSENT){
-    console.log("Employee is ABSENT");
-    return;
-}else{
-    console.log("Employee is PRESENT");
+const IS_PART_TIME = 1;
+const IS_PART_TIME_HOURS = 4;
+const IS_FULL_TIME = 2;
+const IS_FULL_TIME_HOURS = 8;
+const WAGE_PER_HOUR = 20;
+
+let empHrs = 0;
+
+empCheck =  Math.floor(Math.random() * 10) % 3;
+switch(empCheck){
+    case IS_PART_TIME:
+             empHrs = IS_PART_TIME_HOURS;
+            break;
+    case IS_FULL_TIME:
+             empHrs = IS_FULL_TIME_HOURS;
+             break;
+    default :
+             empHrs = 0;
+            break;
 }
+
+let empWage = empHrs * WAGE_PER_HOUR;
+console.log("Emp Wage :" + empWage);
